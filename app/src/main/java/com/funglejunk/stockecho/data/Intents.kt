8a622de156f26a3a8a6630580b9eb936e850e1ba
Michael Marth <michael.marth@joyn.de>
2020-08-29 14:31:20 +0200

    added intent extra field for chart data

--- app/src/main/java/com/funglejunk/stockecho/data/Intents.kt
+++ app/src/main/java/com/funglejunk/stockecho/data/Intents.kt
@@ -3,6 +3,7 @@ package com.funglejunk.stockecho.data
 const val ACTION_REQUEST_UPDATE = "com.funglejunk.stockecho.REQUEST_UPDATE"
 const val ACTION_REPORT_READY = "com.funglejunk.stockecho.REPORT_READY"
 const val EXTRA_REPORT_KEY = "REPORT"
+const val EXTRA_CHART_DATA_KEY = "CHART_DATA"
 const val ACTION_ERROR = "com.funglejunk.stockecho.ERROR"
 const val EXTRA_ERROR_MSG = "MESSAGE"
 
